0690a4b723208fed41e77f1d42a7ce83b5df2ae1
steve <steve.elsens@hotmail.com>
2024-01-31 09:43:15 +0100

    demarrageFooter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    
+ <btn/>
   </div>
 </template>
 
 <script>
+import btn from '@/components/myFooter.vue'
 export default {
+
+  components:{
+    btn
+  }
 
 }
 </script>
