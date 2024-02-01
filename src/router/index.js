16177735f7c08877411cc97ebf7c6d49cf6a0305
Arègue R H <arah@hotmail.fr>
2024-02-01 12:21:17 +0100

    footer quasi finalisé

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MentionsLegales from '../views/MentionsLegales.vue'
+import PolitiqueDeConfidentialite from '../views/PolitiqueDeConfidentialité.vue'
+import ConditionsUtilisation from '../views/ConditionsUtilisation.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  }
+  },
+  { path: '/mentions-legales', component: MentionsLegales },
+  { path: '/politique-de-confidentialite', component: PolitiqueDeConfidentialite },
+  { path: '/conditions-utilisation', component: ConditionsUtilisation }
 ]
 
 const router = createRouter({
