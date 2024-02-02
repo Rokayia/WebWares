8a5220a727318ec9252d0e9dad418b0e2fc91694
Rokayia <rokayia.almhdie@example.com>
2024-02-02 15:32:35 +0100

    ajout icon librairie

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// /* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-// /* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// /* import specific icons */
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-// /* add icons to the library */
-// library.add(faUserSecret)
+/* add icons to the library */
+library.add(faUserSecret)
 
 createApp(App)
 .use(store)
 .use(router)
-// .component('font-awesome-icon', FontAwesomeIcon)
+.component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
